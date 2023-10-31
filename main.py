7b0ab066dd311a596e0eeb7bfb01cd8451f568af
Halip26 <halipuddin.angko@gmail.com>
2023-10-31 19:12:06 +0800

    add comments

--- main.py
+++ main.py
@@ -1,25 +1,32 @@
-# Refactored code
+# Membaca isi file "check.txt"
 with open("check.txt", "r") as f:
     print(f.read())
 
+# Menulis konten ke file "check2.txt"
 with open("check2.txt", "w") as f:
-    f.write("Now the new file is there. With information")
+    f.write("Sekarang file baru ada. Dengan informasi")
 
+# Membaca isi file "check2.txt"
 with open("check2.txt", "r") as f:
     print(f.read())
 
+# Menambahkan teks ke file "check2.txt"
 with open("check2.txt", "a") as f:
     f.write("Now the new file is there. We are appending text in file.")
 
+# Membaca isi file "check2.txt" lagi
 with open("check2.txt", "r") as f:
     print(f.read())
 
+# Membaca dua baris pertama dari file "check.txt"
 with open("check.txt", "r") as f:
     print(f.readline())
     print(f.readline())
 
+# Membaca semua baris dari file "check.txt"
 with open("check.txt", "r") as f:
     print(f.readlines())
 
-with open("the results.txt", "x"):
+# Membuat file kosong "myfile.txt"
+with open("myfile.txt", "x"):
     pass
